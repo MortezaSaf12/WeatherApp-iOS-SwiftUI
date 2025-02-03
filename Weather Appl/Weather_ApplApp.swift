2cd821e8f02a23c6d40ff0938e447bb0a0acda65
MortezaSaf12 <morsaf731@gmail.com>
2025-02-03 10:41:00 +0100

    Initial Commit

---
+++ Weather Appl/Weather_ApplApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ApplApp.swift
+//  Weather Appl
+//
+//  Created by Morteza Safari on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ApplApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather ApplTests/Weather_ApplTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ApplTests.swift
+//  Weather ApplTests
+//
+//  Created by Morteza Safari on 2025-02-03.
+//
+
+import Testing
+@testable import Weather_Appl
+
+struct Weather_ApplTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
